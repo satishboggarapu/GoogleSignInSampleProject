7c28d43042e3194139fe85c66f33346b20440601
Satish Boggarapu <satishbogg9@gmail.com>
2018-12-09 21:08:05 -0500

    Initial Commit

---
+++ GoogleSignInSampleProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleSignInSampleProject
+//
+//  Created by Satish Boggarapu on 12/9/18.
+//  Copyright © 2018 SatishBoggarapu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
